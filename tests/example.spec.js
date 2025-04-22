c7202e8448675d8927a5e3e268024505c2f3cb95
IVY D <71013378+IvyDReyes@users.noreply.github.com>
2025-04-22 22:43:10 +1200

    feat(playwright): add first test with recording and trace viewer support
    - Implemented initial Playwright test to verify page title and navigation
    - Enabled tracing to capture screenshots and snapshots during test execution
    - Included support for trace viewer by saving trace as .zip file for debugging

---
+++ tests/example.spec.js
@@ -0,0 +1,35 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+import path from 'path';
+
+test('has title', async ({ page, context }) => {
+
+
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+
+ 
+
+});
+
+test('get started link', async ({ page, context }) => {
+
+  await context.tracing.start({
+    screenshots: true,
+    snapshots: true
+  });
+
+
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+  await context.tracing.stop({path: 'test2_trace.zip'});
+
+});
